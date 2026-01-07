56362831f7a96a96f67a2b7d1dfc4d5bd447b5bc
AzirML <azir.zero1@gmail.com>
2026-01-07 09:28:14 +0800

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method not allowed' });
   }
   
-  // 返回测试数据
+  // 简单的测试数据，确保能工作
   const testData = [
     { id: 1, name: '红烧肉', description: '经典家常菜' },
     { id: 2, name: '宫保鸡丁', description: '川菜经典' },
@@ -19,5 +19,6 @@ export default async function handler(req, res) {
     { id: 4, name: '水煮鱼', description: '麻辣鲜香' }
   ];
   
-  res.status(200).json(testData);
+  // 返回JSON数据
+  return res.status(200).json(testData);
 }
